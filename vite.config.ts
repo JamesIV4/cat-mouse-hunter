22a64618e6567aff892253b24be7ec88ab48b981
James Pound IV <jamespoundiv@gmail.com>
2025-09-07 18:21:50 -0400

    Updated three.js import

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ export default defineConfig({
   server: {
     open: true,
   },
+  optimizeDeps: {
+    exclude: ["three", "three/examples/jsm/utils/SkeletonUtils.js"],
+  },
   build: {
     outDir: "docs",
+    rollupOptions: {
+      external: ["three", "three/examples/jsm/utils/SkeletonUtils.js"],
+    },
   },
 });
